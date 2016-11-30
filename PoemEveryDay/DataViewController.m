5e9c7ebd3975a9ed88463a5a70e2351da7fa1990
chenjianfeng <chenjianfeng@youmi.net>
2016-11-30 15:38:14 +0800

    bundled change ,bundle name change

--- PoemEveryDay/DataViewController.m
+++ PoemEveryDay/DataViewController.m
@@ -68,17 +68,7 @@
     [detailViewController initControllerWithViewModel:[_viewModel getDetailViewModel]];
 
     [self presentViewController:detailViewController animated:NO completion:^{
-        
     }];
 }
-
-//// In a storyboard-based application, you will often want to do a little preparation before navigation
-//- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-//// Get the new view controller using [segue destinationViewController].
-//// Pass the selected object to the new view controller.
-//    PoemDetailViewController *poemDetailVC = [segue destinationViewController];
-//    [poemDetailVC initControllerWithViewModel:[_viewModel getDetailViewModel]];
-//}
-//
 
 @end
