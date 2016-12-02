92e8abe0d6d4871d794e4f669e1d61fba6b00bbe
chenjianfeng <chenjianfeng@youmi.net>
2016-12-02 17:37:17 +0800

    delete GDT ADS

--- PoemEveryDay/PoemDetailViewController.m
+++ PoemEveryDay/PoemDetailViewController.m
@@ -12,9 +12,8 @@
 #import "PoemDetailDataStruc.h"
 #import "UIImageView+WebCache.h"
 #import "FBKVOController.h"
-#import "GDTMobInterstitial.h"
 
-@interface PoemDetailViewController () <GDTMobInterstitialDelegate>
+@interface PoemDetailViewController ()
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollViewContentBase;
 @property (strong, nonatomic) PoemDetailViewControllerVM *viewModel;
 @property (strong, nonatomic) MBProgressHUD *progressHUD;
@@ -51,7 +50,6 @@
 
 @implementation PoemDetailViewController{
     FBKVOController *_kvoController;
-    GDTMobInterstitial *_interstitialObj;
 }
 
 
@@ -68,7 +66,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    [self initGDTADs];
 }
 
 - (void)viewWillAppear:(BOOL)animated{
@@ -127,107 +124,6 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-#pragma mark - 广点通广告
-
-- (void)initGDTADs{
-    //广点通广告设置
-    _interstitialObj = [[GDTMobInterstitial alloc] initWithAppkey:@"1105190664"
-                                                      placementId:@"4090402818341003"];
-    _interstitialObj.delegate = self; //设置委托
-    _interstitialObj.isGpsOn = NO;     //【可选】设置GPS开关
-    //预加载广告
-    [_interstitialObj loadAd];
-}
-
--(void)showSpot{
-    UIViewController *vc = [[[UIApplication sharedApplication] keyWindow]
-                            rootViewController];
-    int random = arc4random()%3;
-    if (random) {
-        return;
-    }
-    if (_interstitialObj.isReady) {
-        DDLogInfo(@"广点通 ready了");
-        [_interstitialObj presentFromRootViewController:vc];
-    }else{
-        DDLogInfo(@"广点通 还没ready");
-        [_interstitialObj loadAd];
-    }
-}
-
-static NSString *INTERSTITIAL_STATE_TEXT = @"插屏状态";
-/**
- *  广告预加载成功回调
- *  详解:当接收服务器返回的广告数据成功后调用该函数
- */
-- (void)interstitialSuccessToLoadAd:(GDTMobInterstitial *)interstitial
-{
-    NSLog(@"%@:%@",INTERSTITIAL_STATE_TEXT,@"Success Loaded.");
-}
-
-/**
- *  广告预加载失败回调
- *  详解:当接收服务器返回的广告数据失败后调用该函数
- */
-- (void)interstitialFailToLoadAd:(GDTMobInterstitial *)interstitial error:(NSError *)error
-{
-    NSLog(@"%@:%@",INTERSTITIAL_STATE_TEXT,@"Fail Loaded." );
-}
-
-/**
- *  插屏广告将要展示回调
- *  详解: 插屏广告即将展示回调该函数
- */
-- (void)interstitialWillPresentScreen:(GDTMobInterstitial *)interstitial
-{
-    NSLog(@"%@:%@",INTERSTITIAL_STATE_TEXT,@"Going to present.");
-}
-
-/**
- *  插屏广告视图展示成功回调
- *  详解: 插屏广告展示成功回调该函数
- */
-- (void)interstitialDidPresentScreen:(GDTMobInterstitial *)interstitial
-{
-    NSLog(@"%@:%@",INTERSTITIAL_STATE_TEXT,@"Success Presented." );
-}
-
-/**
- *  插屏广告展示结束回调
- *  详解: 插屏广告展示结束回调该函数
- */
-- (void)interstitialDidDismissScreen:(GDTMobInterstitial *)interstitial
-{
-    NSLog(@"%@:%@",INTERSTITIAL_STATE_TEXT,@"Finish Presented.");
-    [_interstitialObj loadAd];
-}
-
-/**
- *  应用进入后台时回调
- *  详解: 当点击下载应用时会调用系统程序打开，应用切换到后台
- */
-- (void)interstitialApplicationWillEnterBackground:(GDTMobInterstitial *)interstitial
-{
-    NSLog(@"%@:%@",INTERSTITIAL_STATE_TEXT,@"Application enter background.");
-}
-
-/**
- *  插屏广告曝光时回调
- *  详解: 插屏广告曝光时回调
- */
--(void)interstitialWillExposure:(GDTMobInterstitial *)interstitial
-{
-    NSLog(@"%@:%@",INTERSTITIAL_STATE_TEXT,@"Exposured");
-}
-/**
- *  插屏广告点击时回调
- *  详解: 插屏广告点击时回调
- */
--(void)interstitialClicked:(GDTMobInterstitial *)interstitial
-{
-    NSLog(@"%@:%@",INTERSTITIAL_STATE_TEXT,@"Clicked");
 }
 
 #pragma mark - UIKVO
